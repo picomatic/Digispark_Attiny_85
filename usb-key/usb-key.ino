f6f5c748b8bd274bb6c8d0a272ef191e35bcd5bb
andre <andre@MacBook-Pro-van-Andre.local>
2021-09-28 19:07:53 +0200

    clean up code

--- usb-key/usb-key.ino
+++ usb-key/usb-key.ino
@@ -6,46 +6,46 @@ void setup() {
   pinMode(0, INPUT);
   pinMode(1, OUTPUT);
   pinMode(2, INPUT);   
-
 }
 
 void loop() 
 {
   while(1)
   {
-  // put your main code here, to run repeatedly:
-  int pass1 = digitalRead(0);
-  int pass2 = digitalRead(2);
+      int pass1 = digitalRead(0);
+      int pass2 = digitalRead(2);
 
-  digitalWrite(1,1);
+      digitalWrite(1,1);
 
-  if(pass1 == 0 && pass2 == 0)
-  {
-      digitalWrite(1,0);
-      DigiKeyboard.sendKeyStroke(KEY_DELETE, MOD_ALT_RIGHT | MOD_CONTROL_LEFT);
-      pass1 = 1;
-      pass2 = 1;
-      DigiKeyboard.delay(1000);
-      continue;
-  }
-  
-  if(pass1 == 0)
-  {
-      digitalWrite(1,0);
-      DigiKeyboard.sendKeyStroke(0);
-      DigiKeyboard.println("yourpassword1");
-      DigiKeyboard.delay(1000);
-      continue;
-  }
-  
-  if(pass2 == 0)
-  {
-      digitalWrite(1,0);
-      DigiKeyboard.sendKeyStroke(0);
-      DigiKeyboard.println("yourpassword2");
-      DigiKeyboard.delay(1000);
-      continue;
-  }
-  DigiKeyboard.delay(100);
+      if(pass1 == 0 && pass2 == 0)
+      {
+           DigiKeyboard.sendKeyStroke(KEY_DELETE, MOD_ALT_RIGHT | MOD_CONTROL_LEFT);
+           KeyPressed();
+      } 
+      else if(pass1 == 0)
+      {   
+           DigiKeyboard.println("yourpassword1");
+           KeyPressed();
+      } 
+      else if(pass2 == 0)
+      {
+          DigiKeyboard.println("yourpassword2");
+          KeyPressed();
+      } 
+      else
+      {
+      }
+
+      DigiKeyboard.delay(100);
   }
 }
+
+void KeyPressed()
+{
+    digitalWrite(1,0);
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.delay(250);
+}
+
+
+
